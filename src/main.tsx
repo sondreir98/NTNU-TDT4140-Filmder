d7d3f5c27a784a3c10a4f5e8ca1dfb66250f515b
Vegard Kyrkjedelen Hovland <vegard@kyrkjedelen.no>
2025-01-29 17:48:23 +0100

    build: :building_construction: Add more packages
    Added tailwind and biome and router packages. Personilized the App, and removed unneeded Vite files and code.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(
+	document.querySelector(":root > body > #react-root") as HTMLDivElement,
+).render(
+	<StrictMode>
+		<App />
+	</StrictMode>,
+);
