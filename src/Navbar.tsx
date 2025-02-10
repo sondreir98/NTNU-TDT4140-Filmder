db128987a8cd18bc358125b1626e89dc8e164b8b
Camilla Eriksen <camilla.eriksen@sf-nett.no>
2025-02-10 16:29:24 +0100

    feat: :sparkles: add navbar
    Made a navbar file

---
+++ src/Navbar.tsx
@@ -0,0 +1,10 @@
+function Navbar() {
+    return (
+        <nav className="fixed bottom-0 w-full bg-white shadow-lg p-3">
+            <div className="flex justify-around items-center">
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import Home from "./Home.tsx";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./Navbar.tsx";
 
 createRoot(
 	document.querySelector(":root > body > #react-root") as HTMLDivElement,
@@ -11,6 +12,7 @@ createRoot(
 		<BrowserRouter>
 			<Routes>
 				<Route index element={<Home/>} />
+				<Navbar/>
 			</Routes>
 		</BrowserRouter>
 	</StrictMode>,
