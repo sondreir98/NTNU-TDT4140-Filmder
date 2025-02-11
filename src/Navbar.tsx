eec96e06f9566cddd61bf38642249af8f6c5129f
Camilla Eriksen <camilla.eriksen@sf-nett.no>
2025-02-11 11:31:29 +0100

    style: :lipstick: styled navbar

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -6,15 +6,15 @@ function Navbar() {
     return (
         <nav className="fixed bottom-0 w-full bg-white shadow-lg p-10">
         <div className="flex justify-around items-center -space-x-150">
-            <Link to="/profile" className="flex flex-col items-center text-dark hover:text-blue-500">
+            <Link to="/Friends" className="flex flex-col items-center text-dark hover:text-blue-500">
                 <UsersIcon className="h-6 w-6" />
                 <span className="text-sm">Friends</span>
             </Link>
-            <Link to="/" className="flex flex-col items-center text-dark hover:text-blue-500">
+            <Link to="/Home" className="flex flex-col items-center text-dark hover:text-blue-500">
                 <HomeIcon className="h-6 w-6" />
                 <span className="text-sm">Home</span>
             </Link>
-            <Link to="/settings" className="flex flex-col items-center text-dark hover:text-blue-500">
+            <Link to="/Search" className="flex flex-col items-center text-dark hover:text-blue-500">
                 <MagnifyingGlassIcon className="h-6 w-6" />
                 <span className="text-sm">Search</span>
             </Link>
