8d7b8e40fc8dbfef4826e20749b0f14a46c65379
Sondre Ingvaldsen Rognli <sondreir@stud.ntnu.no>
2025-02-14 20:14:35 +0100

    feat: :sparkles: add form validation

--- src/pages/loginpage.tsx
+++ src/pages/loginpage.tsx
@@ -1,11 +1,28 @@
+import { FirebaseError } from "firebase/app";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { auth } from "../Database";
 
 const LoginPage = () => {
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
+	const [error, setError] = useState("");
+	const navigate = useNavigate();
+
+	const handleLogin = async () => {
+		try {
+			await signInWithEmailAndPassword(auth, email, password);
+			setError("");
+			navigate("/");
+		} catch (err) {
+			if (err instanceof FirebaseError) {
+				setError("The email or password you provided is incorrect.");
+			} else {
+				setError("An unknown error occurred.");
+			}
+		}
+	};
 
 	return (
 		//Fargene endres til slutt
@@ -16,7 +33,7 @@ const LoginPage = () => {
 				placeholder="Email"
 				value={email}
 				onChange={(e) => setEmail(e.target.value)}
-				className="shadow mb-2 px-4 py-4 rounded-[2vw] w-80 mt-90 text-dark font-semibold bg-primary focus:outline-none focus:ring-2 focus:ring-dark ease-linear transition duration-200 "
+				className="shadow mb-4 px-4 py-4 rounded-[2vw] w-80 mt-70 text-dark font-semibold bg-primary focus:outline-none focus:ring-2 focus:ring-dark ease-linear transition duration-200 "
 			/>
 			<input
 				type="password"
@@ -25,22 +42,26 @@ const LoginPage = () => {
 				onChange={(e) => setPassword(e.target.value)}
 				className="shadow mb-4 px-4 py-4 w-80 rounded-[2vw] bg-primary text-dark font-semibold focus:outline-none focus:ring-2 focus:ring-dark ease-linear transition duration-200"
 			/>
+
 			<div className="flex flex-col gap-4">
-				<Link
-					to="/"
-					onClick={async () => {
-						await signInWithEmailAndPassword(auth, email, password);
-					}}
+				<button
+					type="button"
+					onClick={handleLogin}
 					className="border-2 border-dark w-80 px-6 py-3 rounded-[2vw] text-lg font-semibold shadow-md bg-primary text-dark hover:bg-gray-200 transition"
 				>
 					Log In
-				</Link>
-				<div className="text-center mt-2">
+				</button>
+				{error && (
+					<div className="flex flex-col items-center justify-center text-red-500 font-semibold text-sm mt-1">
+						<p>{error}</p>
+					</div>
+				)}
+				<div className="text-center mt-0">
 					<p className="text-sm">
 						Don't have an account?{" "}
 						<Link
 							to="/signup"
-							className="text-blue-400 font-semibold hover:underline"
+							className="text-blue-400 font-bold hover:underline"
 						>
 							Sign Up
 						</Link>
