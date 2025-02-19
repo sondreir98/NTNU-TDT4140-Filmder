749571cb788d85233d3a82c2a76656272de5415c
Vegard Kyrkjedelen Hovland <vegard@kyrkjedelen.no>
2025-02-19 19:39:32 +0100

    style: :rotating_light: Last pnpm check
    Fixed semicolon in datbase and spacing in profile page button.

--- src/ProfilePageButton.tsx
+++ src/ProfilePageButton.tsx
@@ -1,20 +1,20 @@
 import { useNavigate } from "react-router-dom";
 
 const ProfilePageButton: React.FC = () => {
-    const navigate = useNavigate();
-    return (
-        <button
-            onClick={() => navigate("/Profile")}
-            type="button"
-            className="top-10 right-10 bg-primary text-white px-4 py-2 rounded-lg hover:opacity-80 transition cursor-pointer"
-        >
-            <img
-                src="https://www.svgrepo.com/show/343494/profile-user-account.svg"
-                alt="Go to profile"
-                className="w-4 h-4"
-            />
-        </button>
-    );
+	const navigate = useNavigate();
+	return (
+		<button
+			onClick={() => navigate("/Profile")}
+			type="button"
+			className="top-10 right-10 bg-primary text-white px-4 py-2 rounded-lg hover:opacity-80 transition cursor-pointer"
+		>
+			<img
+				src="https://www.svgrepo.com/show/343494/profile-user-account.svg"
+				alt="Go to profile"
+				className="w-4 h-4"
+			/>
+		</button>
+	);
 };
 
 export default ProfilePageButton;
